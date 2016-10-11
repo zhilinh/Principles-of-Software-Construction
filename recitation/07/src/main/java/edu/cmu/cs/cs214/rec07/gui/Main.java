990cc85511a24d2cfd58e5505455e283892523ec
Charlie Garrod <charlie@cs.cmu.edu>
2016-10-11 11:50:51 -0400

    Staff add recitation 07 [ci skip]

---
+++ recitation/07/src/main/java/edu/cmu/cs/cs214/rec07/gui/Main.java
@@ -0,0 +1,26 @@
+package edu.cmu.cs.cs214.rec07.gui;
+
+import edu.cmu.cs.cs214.rec07.solver.TheBestCryptarithmSolver;
+
+import javax.swing.*;
+
+/**
+ * Entry point of your program
+ */
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            /*
+               TODO:
+
+               create an instance of your CryptarithmSolverGui backed by a new instance of
+               TheBestCryptarithmSolver and make it visible. This requires a single line of code:
+
+               https://docs.oracle.com/javase/8/docs/api/java/awt/Component.html#setVisible-boolean-
+
+             */
+
+             // Your code goes here
+        });
+    }
+}
